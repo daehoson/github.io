c41183a878ffc03306050f9e13d187d1fb34131a
daehoson <daehoson@naver.com>
2025-04-27 11:44:04 +0900

    - 맵API보이기 - 수정중EN

--- assets/js/main.js
+++ assets/js/main.js
@@ -135,8 +135,8 @@
     new Typed('.typed', {
       strings: typed_strings,
       loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
+      typeSpeed: 50,
+      backSpeed: 30,
       backDelay: 2000
     });
   }
